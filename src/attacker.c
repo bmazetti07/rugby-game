d1b5885f93c9aaef280ce307c6f7cf1410d66813
bmazetti07 <bruno.mazetti@usp.br>
2022-03-26 21:14:14 -0300

    Estratégias do atacante e do defensor implementadas

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -11,19 +13,84 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define TURNO_SPY 5
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+direction_t execute_attacker_strategy(position_t attacker_position, Spy defender_spy) {
+  static int turno = 0;
+  static position_t pos_atual, pos_anterior;
+  static position_t defender_position;
+  direction_t resultado = (direction_t) DIR_STAY;
+  
+  pos_atual = attacker_position;
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  if (turno == 0) {
+    srand(time(0));
+    pos_anterior = attacker_position;
+    defender_position.i = defender_position.j = 0;
+  }
+
+  if (turno == TURNO_SPY) {
+    defender_position = get_spy_position(defender_spy);
+  }
+
+  while ((pos_anterior.i == pos_atual.i) && (pos_anterior.j == pos_atual.j)) {
+    int x = rand() % 100;
+
+
+    if (x < 25) {
+      pos_atual.j += 1;
+      resultado = (direction_t) DIR_RIGHT;
+    }
+    else if (x < 50) {
+      pos_atual.i += -1;
+      pos_atual.j += 1;
+      resultado = (direction_t) DIR_UP_RIGHT;
+    }
+    else if (x < 75) {
+      pos_atual.i += 1;
+      pos_atual.j += 1;
+      resultado = (direction_t) DIR_DOWN_RIGHT;
+    }
+    else if (x < 80) {
+      pos_atual.i += -1;
+      resultado = (direction_t) DIR_UP;
+    }
+    else if (x < 85) {
+      pos_atual.i += 1;
+      resultado = (direction_t) DIR_DOWN;
+    }
+    else if (x < 90) {
+      pos_atual.i += -1;
+      pos_atual.j += -1;
+      resultado = (direction_t) DIR_UP_LEFT;
+    }
+    else if (x < 95) {
+      pos_atual.i += 1;
+      pos_atual.j += -1;
+      resultado = (direction_t) DIR_DOWN_LEFT;
+    }
+    else if (x < 99) {
+      pos_atual.j += -1;
+      resultado = (direction_t) DIR_LEFT;
+    }
+    else {
+      resultado = (direction_t) DIR_STAY;
+      break;
+    }
+
+    if ((turno == TURNO_SPY) &&
+      (abs((int) (pos_atual.i - defender_position.i)) + abs((int) (pos_atual.j - defender_position.j)) <
+      abs((int) (pos_anterior.i - defender_position.i)) + abs((int) (pos_anterior.j - defender_position.j)))) {
+          pos_atual = pos_anterior;
+    }
+  }
+  pos_anterior = pos_atual;
+  turno ++;
+  return resultado;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,9 +1,13 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
 #include "position.h"
+#include "dimension.h"
+#include "field.h"
 #include "spy.h"
 
 // Main header
@@ -11,19 +15,74 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define TURNO_SPY 5
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+direction_t execute_defender_strategy(position_t defender_position, Spy attacker_spy) {
+  static int turno = 0;
+  static position_t pos_atual, pos_anterior;
+  static position_t attacker_position;
+  direction_t resultado = (direction_t) DIR_STAY;
+  
+  pos_atual = defender_position;
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  if (turno == 0) {
+    srand(time(0));
+    pos_anterior = defender_position;
+    pos_atual.j += -1;
+    resultado = (direction_t) DIR_LEFT;
+    attacker_position.i = attacker_position.j = 0;
+  }
+
+  else {
+
+    if (turno == TURNO_SPY) {
+      attacker_position = get_spy_position(attacker_spy);
+    }
+    //printf("valor da variavel estática: %d\n", turno);
+
+    while ((pos_anterior.i == pos_atual.i) && (pos_anterior.j == pos_atual.j)) {
+      if (defender_position.i == 1) {
+        pos_atual.i += 1;
+        resultado = (direction_t) DIR_DOWN;
+      }
+      else if (defender_position.i == 8) {
+        pos_atual.i += -1;
+        resultado = (direction_t) DIR_UP; 
+      }
+
+      else {
+        int x = random() % 100;
+
+        if (x < 40) {
+          pos_atual.i += -1;
+          resultado = (direction_t) DIR_UP;
+        }
+        else if (x < 80) {
+          pos_atual.i += 1;
+          resultado = (direction_t) DIR_DOWN;
+        }
+
+        else {
+          resultado = (direction_t) DIR_STAY;
+          break;
+        }
+      }
+
+      if ((turno == TURNO_SPY) &&
+        (abs((int) (pos_atual.i - attacker_position.i)) + abs((int) (pos_atual.j - attacker_position.j)) >
+        abs((int) (pos_anterior.i - attacker_position.i)) + abs((int) (pos_anterior.j - attacker_position.j)))) {
+            pos_atual = pos_anterior;
+      }
+    }
+  }
+
+  pos_anterior = pos_atual;
+  turno ++;
+  return resultado;
 }
 
 /*----------------------------------------------------------------------------*/
